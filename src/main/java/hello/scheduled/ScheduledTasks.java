18a66420515b0eb547c0468b5971d024f12185de
BBK-PiJ-2015-87 <ivanovs223@gmail.com>
2016-10-05 15:58:03 +0100

    Added scheduled task.

---
+++ src/main/java/hello/scheduled/ScheduledTasks.java
@@ -0,0 +1,27 @@
+package hello.scheduled;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * This class should be in the same directory as Main class (Application)
+ */
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat date = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportTime(){
+        LOG.info("Current time is {}", date.format(new Date()));
+    }
+
+}
